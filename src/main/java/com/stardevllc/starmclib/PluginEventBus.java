322237bd4abc9877171f132e05c890b5d85f2057
Firestar311 <slaterandrew0398@gmail.com>
2025-07-08 16:03:54 -0500

    Added an IEventBus that is plugin specific

---
+++ src/main/java/com/stardevllc/starmclib/PluginEventBus.java
@@ -0,0 +1,20 @@
+package com.stardevllc.starmclib;
+
+import com.stardevllc.eventbus.impl.SimpleEventBus;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * An {@link com.stardevllc.eventbus.IEventBus} that allows any object to be called
+ * @param <P> The plugin type
+ */
+public class PluginEventBus<P extends JavaPlugin> extends SimpleEventBus<Object> {
+    protected final P plugin;
+    
+    public PluginEventBus(P plugin) {
+        this.plugin = plugin;
+    }
+    
+    public P getPlugin() {
+        return plugin;
+    }
+}
