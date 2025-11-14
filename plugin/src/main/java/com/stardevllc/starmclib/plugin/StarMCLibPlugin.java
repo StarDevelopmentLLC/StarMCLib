c3be166bddb155dcbf3737ed47934cf1c5b8dad1
Firestar311 <slaterandrew0398@gmail.com>
2025-11-14 17:24:54 -0600

    Revert the libby changes - Causing issues downstream

--- plugin/src/main/java/com/stardevllc/starmclib/plugin/StarMCLibPlugin.java
+++ plugin/src/main/java/com/stardevllc/starmclib/plugin/StarMCLibPlugin.java
@@ -2,16 +2,8 @@ package com.stardevllc.starmclib.plugin;
 
 import com.stardevllc.starmclib.StarMCLib;
 import com.stardevllc.starmclib.smlplugin.cmd.StarMCLibCmd;
-import net.byteflux.libby.BukkitLibraryManager;
-
-import java.io.File;
 
 public class StarMCLibPlugin extends ExtendedJavaPlugin {
-    public StarMCLibPlugin() {
-        BukkitLibraryManager bukkitLibraryManager = new BukkitLibraryManager(this, new File(".", "plugins").toPath(), "libraries");
-        bukkitLibraryManager.configureFromJSON();
-    }
-    
     @Override
     public void onEnable() {
         StarMCLib.init(this);
